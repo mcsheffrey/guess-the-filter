c5390217f1e1867875e49c44f6ec3944d1a70c06
Connor McSheffrey <connor.mcsheffrey@gmail.com>
2013-09-16 15:26:30 -0700

    added additional webfonts, backbone.js, firebase

--- js/app.js
+++ js/app.js
@@ -1,24 +1,81 @@
 /** @jsx React.DOM */
+var LEADERBOARD_SIZE = 5;
 
-var Utils = {
-  // https://gist.github.com/1308368
-  uuid: function(a,b){for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b},
-  pluralize: function( count, word ) {
-    return count === 1 ? word : word + 's';
-  },
-  store: function( namespace, data ) {
-    if ( arguments.length > 1 ) {
-      return localStorage.setItem( namespace, JSON.stringify( data ) );
-    } else {
-      var store = localStorage.getItem( namespace );
-      return ( store && JSON.parse( store ) ) || [];
-    }
+// Create our Firebase reference
+var scoreListRef = new Firebase('https://guess-the-filter.firebaseIO.com///scoreList');
+
+// Keep a mapping of firebase locations to HTML elements, so we can move / remove elements as necessary.
+var htmlForPath = {};
+
+// Helper function that takes a new score snapshot and adds an appropriate row to our leaderboard table.
+function handleScoreAdded(scoreSnapshot, prevScoreName) {
+  var newScoreRow = $("<tr/>");
+  newScoreRow.append($("<td/>").append($("<em/>").text(scoreSnapshot.val().name)));
+  newScoreRow.append($("<td/>").text(scoreSnapshot.val().score));
+
+  // Store a reference to the table row so we can get it again later.
+  htmlForPath[scoreSnapshot.name()] = newScoreRow;
+
+  // Insert the new score in the appropriate place in the table.
+  if (prevScoreName === null) {
+    $("#leaderboardTable").append(newScoreRow);
   }
+  else {
+    var lowerScoreRow = htmlForPath[prevScoreName];
+    lowerScoreRow.before(newScoreRow);
+  }
+}
+
+// Helper function to handle a score object being removed; just removes the corresponding table row.
+function handleScoreRemoved(scoreSnapshot) {
+  var removedScoreRow = htmlForPath[scoreSnapshot.name()];
+  removedScoreRow.remove();
+  delete htmlForPath[scoreSnapshot.name()];
+}
+
+// Create a view to only receive callbacks for the last LEADERBOARD_SIZE scores
+var scoreListView = scoreListRef.limit(LEADERBOARD_SIZE);
+
+// Add a callback to handle when a new score is added.
+scoreListView.on('child_added', function (newScoreSnapshot, prevScoreName) {
+  handleScoreAdded(newScoreSnapshot, prevScoreName);
+});
+
+// Add a callback to handle when a score is removed
+scoreListView.on('child_removed', function (oldScoreSnapshot) {
+  handleScoreRemoved(oldScoreSnapshot);
+});
+
+// Add a callback to handle when a score changes or moves positions.
+var changedCallback = function (scoreSnapshot, prevScoreName) {
+  handleScoreRemoved(scoreSnapshot);
+  handleScoreAdded(scoreSnapshot, prevScoreName);
 };
+scoreListView.on('child_moved', changedCallback);
+scoreListView.on('child_changed', changedCallback);
 
+// When the user presses enter on scoreInput, add the score, and update the highest score.
+$("#scoreInput").keypress(function (e) {
+  if (e.keyCode == 13) {
+    var newScore = Number($("#scoreInput").val());
+    var name = $("#nameInput").val();
+    $("#scoreInput").val("");
 
-var Comment = React.createClass({
+    if (name.length === 0)
+      return;
+
+    var userScoreRef = scoreListRef.child(name);
+    
+    // Use setWithPriority to put the name / score in Firebase, and set the priority to be the score.
+    userScoreRef.setWithPriority({ name:name, score:newScore }, newScore);
+  }
+});
+  
+
+var Media = React.createClass({
   render: function() {
+    console.log('this', this._domIndex);
+    
     return (
       React.DOM.div({
         className: 'photo-container ' + this.props.class_transition,
@@ -58,7 +115,7 @@ var FilterChooser = React.createClass({
     }
   });
 
-var CommentBox = React.createClass({
+var GuessTheFilter = React.createClass({
   config: {
     igPhotoFilters: ['Amaro','Mayfair','Rise','Valencia','Hudson','X-Pro II','Sierra','Willow','Lo-fi','Earlybird','Sutro','Toaster','Brannan','Inkwell','Walden','Hefe','Nashville','1977','Kelvin'],
     igVideoFilters: ['Stinson', 'Vespar', 'Clarendon', 'Maven', 'Gingham', 'Ginza', 'Skyline', 'Dogpatch', 'Brooklyn', 'Moon', 'Helena', 'Ashby', 'Charmes'],
@@ -120,11 +177,14 @@ var CommentBox = React.createClass({
               console.log(this.config.igPhotoFilters, results.data[i].filter);
               
               randomFilter = this.pickRandomFilter(this.config.igPhotoFilters, results.data[i].filter);
+              console.log('image', randomFilter);
+              
             }
 
             // add random filter if video
             if (results.data[i].type === "video") {
               randomFilter = this.pickRandomFilter(this.config.igVideoFilters, results.data[i].filter);
+              console.log('video', randomFilter);
             }
 
             console.log(randomFilter);
@@ -213,14 +273,14 @@ var CommentBox = React.createClass({
     var item = items[Math.floor(Math.random()*items.length)];
 
     // make sure fake filter is not the same as real filter
-    if (item !== exceptionItem) {
+    if (item.localeCompare(exceptionItem)) {
       return item;
     } else {
-      return this.pickRandomFilter(item, exceptionItem);
+      return this.pickRandomFilter(items, exceptionItem);
     }
   },
 
-  loadCommentsFromServer: function() {
+  loadMediaFromServer: function() {
 
     this.fetch();
 
@@ -240,10 +300,12 @@ var CommentBox = React.createClass({
     };
   },
   componentDidMount: function() {
-    this.loadCommentsFromServer();
-    // setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    this.loadMediaFromServer();
+    // setInterval(this.loadMediaFromServer, this.props.pollInterval);
   },
   makeSelection: function(filter) {
+    var self = this;
+
     console.log('event', this);
     console.log($(this.getDOMNode()));
     
@@ -256,33 +318,37 @@ var CommentBox = React.createClass({
 
       this.state.score.correct++;
 
-    };
+      this.setState({
+        transition: 'state-correct fadeIn'
+      });
+    } else {
+      this.setState({
+        transition: 'state-incorrect fadeIn'
+      });
+    }
 
     this.state.score.total++;
 
     console.log(this.state);
 
-    this.setState({
-      transition: 'animate-fly-out-left'
-    });
+    setTimeout(function() {
+      self.state.transition = '';
 
-    // setTimeout(function() {
-    //   this.state.data[0].stateTransition;
-    // }, milliseconds);
+      self.setState({
+        data: self.state.data.slice(1)
+      });
 
-    this.setState({
-      data: this.state.data.slice(1)
-    });
+      self.setState({current: self.state.data[0].filter});
 
-    this.setState({current: this.state.data[0].filter});
+      self.setState({filter: [self.state.data[0].filter, self.state.data[0].fake_filter]});
 
-    this.setState({filter: [this.state.data[0].filter, this.state.data[0].fake_filter]});
-
-    if (this.state.data.length < 4) {
-      
-      this.fetch();
-    };
+      if (self.state.data.length < 4) {
+        
+        self.fetch();
+      };
     
+    }, 1000);
+
   },
   render: function() {
     var self = this,
@@ -303,7 +369,7 @@ var CommentBox = React.createClass({
         </header>
         <div class="main">
           <div class="wrap">
-            <CommentList data={this.state.data} transition={this.state.transition} />
+            <MediaList data={this.state.data} transition={this.state.transition} />
 
             <div class="filter-items">{filterNodes}</div>
             <span class="seperator">or</span>
@@ -316,19 +382,19 @@ var CommentBox = React.createClass({
 });
 
 
-var CommentList = React.createClass({
+var MediaList = React.createClass({
   render: function() {
     var self = this,
-        commentNodes = this.props.data.map(function (comment) {
+        mediaNodes = this.props.data.map(function (media) {
       return (
-        <Comment img_url={comment.img_url} media_id={comment.media_id} photo_index={comment.photo_index} class_transition={self.props.transition} />
+        <Media img_url={media.img_url} media_id={media.media_id} photo_index={media.photo_index} class_transition={self.props.transition} />
       );
     });
-    return <div class="images-wrapper">{commentNodes}</div>;
+    return <div class="images-wrapper">{mediaNodes}</div>;
   }
 });
 
 React.renderComponent(
-  <CommentBox />,
+  <GuessTheFilter />,
   document.getElementById('output')
 );
--- js/plugins.js
+++ js/plugins.js
@@ -19,6 +19,23 @@
             console[method] = noop;
         }
     }
+
+    var Utils = {
+      // https://gist.github.com/1308368
+      uuid: function(a,b){for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b},
+      pluralize: function( count, word ) {
+        return count === 1 ? word : word + 's';
+      },
+      store: function( namespace, data ) {
+        if ( arguments.length > 1 ) {
+          return localStorage.setItem( namespace, JSON.stringify( data ) );
+        } else {
+          var store = localStorage.getItem( namespace );
+          return ( store && JSON.parse( store ) ) || [];
+        }
+      }
+    };
+
 }());
 
 // Place any jQuery/helper plugins in here.
