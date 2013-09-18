266d2c643ed0bd8c03bb04c6c20a5c3f919ec668
Connor McSheffrey <connor.mcsheffrey@gmail.com>
2013-09-17 22:19:14 -0700

    Make it responsive yo!

--- js/app.js
+++ js/app.js
@@ -1,76 +1,4 @@
 /** @jsx React.DOM */
-var LEADERBOARD_SIZE = 5;
-
-// Create our Firebase reference
-var scoreListRef = new Firebase('https://guess-the-filter.firebaseIO.com///scoreList');
-
-// Keep a mapping of firebase locations to HTML elements, so we can move / remove elements as necessary.
-var htmlForPath = {};
-
-// Helper function that takes a new score snapshot and adds an appropriate row to our leaderboard table.
-function handleScoreAdded(scoreSnapshot, prevScoreName) {
-  var newScoreRow = $("<tr/>");
-  newScoreRow.append($("<td/>").append($("<em/>").text(scoreSnapshot.val().name)));
-  newScoreRow.append($("<td/>").text(scoreSnapshot.val().score));
-
-  // Store a reference to the table row so we can get it again later.
-  htmlForPath[scoreSnapshot.name()] = newScoreRow;
-
-  // Insert the new score in the appropriate place in the table.
-  if (prevScoreName === null) {
-    $("#leaderboardTable").append(newScoreRow);
-  }
-  else {
-    var lowerScoreRow = htmlForPath[prevScoreName];
-    lowerScoreRow.before(newScoreRow);
-  }
-}
-
-// Helper function to handle a score object being removed; just removes the corresponding table row.
-function handleScoreRemoved(scoreSnapshot) {
-  var removedScoreRow = htmlForPath[scoreSnapshot.name()];
-  removedScoreRow.remove();
-  delete htmlForPath[scoreSnapshot.name()];
-}
-
-// Create a view to only receive callbacks for the last LEADERBOARD_SIZE scores
-var scoreListView = scoreListRef.limit(LEADERBOARD_SIZE);
-
-// Add a callback to handle when a new score is added.
-scoreListView.on('child_added', function (newScoreSnapshot, prevScoreName) {
-  handleScoreAdded(newScoreSnapshot, prevScoreName);
-});
-
-// Add a callback to handle when a score is removed
-scoreListView.on('child_removed', function (oldScoreSnapshot) {
-  handleScoreRemoved(oldScoreSnapshot);
-});
-
-// Add a callback to handle when a score changes or moves positions.
-var changedCallback = function (scoreSnapshot, prevScoreName) {
-  handleScoreRemoved(scoreSnapshot);
-  handleScoreAdded(scoreSnapshot, prevScoreName);
-};
-scoreListView.on('child_moved', changedCallback);
-scoreListView.on('child_changed', changedCallback);
-
-// When the user presses enter on scoreInput, add the score, and update the highest score.
-$("#scoreInput").keypress(function (e) {
-  if (e.keyCode == 13) {
-    var newScore = Number($("#scoreInput").val());
-    var name = $("#nameInput").val();
-    $("#scoreInput").val("");
-
-    if (name.length === 0)
-      return;
-
-    var userScoreRef = scoreListRef.child(name);
-    
-    // Use setWithPriority to put the name / score in Firebase, and set the priority to be the score.
-    userScoreRef.setWithPriority({ name:name, score:newScore }, newScore);
-  }
-});
-
 
 var Media = React.createClass({
   render: function() {
@@ -88,9 +16,7 @@ var Media = React.createClass({
           style: {
             backgroundImage: "url(" + this.props.img_url + ")"
           },
-          id: '{this.props.id}',
-          height: '612',
-          width: '612'
+          id: '{this.props.id}'
         })
       )
     );
